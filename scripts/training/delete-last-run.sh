c85b928967e179d175706309e445b92d54e361df
Jerome Hourquebie <jerome.hourquebie@gmail.com>
2020-03-24 13:39:10 +0000

    few fixes

--- scripts/training/delete-last-run.sh
+++ scripts/training/delete-last-run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-rm -rf ../../docker/volumes/minio/bucket/rl-deepracer-sagemaker
-rm -rf ../../docker/volumes/robo/checkpoint/checkpoint
+sudo rm -rf ../../docker/volumes/minio/bucket/rl-deepracer-sagemaker
+sudo rm -rf ../../docker/volumes/robo/checkpoint/checkpoint
 mkdir ../../docker/volumes/robo/checkpoint/checkpoint
-rm -rf /robo/container/*
-rm -rf ../../docker/volumes/robo/checkpoint/log/*
+sudo rm -rf /robo/container/*
+sudo rm -rf ../../docker/volumes/robo/checkpoint/log/*
