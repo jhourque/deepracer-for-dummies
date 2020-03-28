956c67a12302fdc608e319086165447c0fb8371b
Jerome Hourquebie <jerome.hourquebie@gmail.com>
2020-03-28 16:55:20 +0000

    small fix

--- scripts/training/set-last-run-to-pretrained.sh
+++ scripts/training/set-last-run-to-pretrained.sh
@@ -3,7 +3,7 @@ Folder=rl-deepracer-sagemaker
 if [ -d ../../docker/volumes/minio/bucket/rl-deepracer-sagemaker ];
 then
 	echo "Folder $Folder  exist."
-	rm -rf ../../docker/volumes/minio/bucket/rl-deepracer-pretrained
+	sudo rm -rf ../../docker/volumes/minio/bucket/rl-deepracer-pretrained
 	mv ../../docker/volumes/minio/bucket/rl-deepracer-sagemaker ../../docker/volumes/minio/bucket/rl-deepracer-pretrained
 	echo "Done."
 
